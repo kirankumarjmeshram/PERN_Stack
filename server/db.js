06fdcb38fa6c66181d794f7b05bcfec325f53dd3
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-08-21 18:05:15 +0530

    added db.js

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: "postgress",
+    password:"123abc",
+    host:"localhost",
+    port: 5001,
+    database: "perntodo"
+});
+
+module.exports = pool;
